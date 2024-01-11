3d61ce9e3fe86ebe8843ca29032680edc5982f31
kimhyesom <kimhyesom1202@gmail.com>
2024-01-11 18:16:01 +0900

    💄 Setup reset & theme colors

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,7 +1,29 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle`
+  ${reset}
+  
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  };
 
+  a {
+    color: inherit;
+    text-decoration: none;
+  };
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: inherit;
+    border: none;
+    outline: none;
+    cursor: pointer;
+  }
 `;
 
 export default GlobalStyles;
